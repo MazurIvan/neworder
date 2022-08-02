40dd09a91f94b3b5af7b160bd6f848426130db51
Ivan Mazur <ivanmazurua@gmail.com>
2022-08-02 16:29:41 +0300

    new2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import "./style.css";
 function App() {
 
   const [count, updateCount] = useState(0);
+  const [basket, updateBasket] = useState(0)
+
+  const addBasket = () => {
+    updateBasket(basket + 1);
+  }
 
   const handleClick = () => {
     updateCount(count + 1);
@@ -15,13 +20,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Борщ Український</h1>
+      <h1>Борщ</h1>
+      <h2>100грн</h2>
       <p>Кількість замовленних порцій: {count}.</p>
       <button onClick={handleClick} className="Add">Додати</button>
       <button onClick={handleClickMin} className="Del">Прибрати</button>
       <br></br>
       <br></br>
-      <button className="Buy">В кошик!</button>
+      <button onClick={addBasket} className="Buy">У кошик!</button>
+      <br></br>
+      <p className="Basket">У кошику {basket} замовлень.</p>
     </div>
   );
 }
