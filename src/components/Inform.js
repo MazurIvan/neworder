6ce1a12228e3f76e94be01b9c24ae3596b78dcf4
Ivan Mazur <ivanmazurua@gmail.com>
2022-08-30 22:50:40 +0300

    add_ua

--- src/components/Inform.js
+++ src/components/Inform.js
@@ -7,19 +7,21 @@ export class Inform extends Component {
   render() {
     return (
       <div className='inform'>
-        <div id='sec1' className='about-us'>
+        <div className='ornament' id='sec1'>
+        </div>
+        <div id='sec2' className='about-us'>
             <h1>Про нас</h1>
             <br></br>
             <p>Тут умовний опис умовної доставки. Або ресторану. Самі щось нафантазуйте. Адресу, наприклад. Корочше, літри води у вигляді нахвалювань.</p>
         </div>
-        <div id='sec2' className='contacts-us'>
+        <div id='sec3' className='contacts-us'>
             <h1>Контакти</h1>
             <br></br>
             <FaFacebook className='contacts' />
             <FaInstagram className='contacts' />
             <FaTelegram className='contacts' />
         </div>
-        <h1 id='sec3'>Відгуки</h1>
+        <h1>Відгуки</h1>
       </div>
     )
   }
